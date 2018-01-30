ef8c355454ec5c8d4118d02c841b8a149fab82a6
Zach <dobbszach@gmail.com>
2018-01-30 17:47:22 -0600

    initial commit

---
+++ Swift-Basics/Swift-Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Basics
+//
+//  Created by Zachary Dobbs on 1/30/18.
+//  Copyright © 2018 Zachary Dobbs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
